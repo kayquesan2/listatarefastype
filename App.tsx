268526a5ec4125764d033d4db8f576d4a8d7c4cf
kayquesan2 <rm552605@fiap.com.br>
2024-09-02 14:33:17 -0300

    Atualiza a configuração inicial do app com Native Base

--- App.tsx
+++ App.tsx
@@ -1,22 +1,16 @@
-import { StatusBar } from 'expo-status-bar';  //componenente do expo que permite controlar o estilo da barra de status do sistema, como hora, bateria, etc.
-
-import { StyleSheet, Text, View } from 'react-native'; //componentes básicos do react native.
-
+import React from 'react';
+import { NativeBaseProvider, Box, Text, Center } from 'native-base';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NativeBaseProvider>
+      <Center flex={1} bg="white">
+        <Box>
+          <Text fontSize="2xl" color="primary.500" fontWeight="bold" fontFamily="Roboto">
+            Olá TDSZ, nosso app está configurado com Native Base!
+          </Text>
+        </Box>
+      </Center>
+    </NativeBaseProvider>
   );
 }
-
-const styles = StyleSheet.create({  //cria um objeto de estilos para ser usado no aplicativo. É aplicado na View.
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
